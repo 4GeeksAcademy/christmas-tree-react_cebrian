25a6078548aa57ec68d1ad72733f7d21cebe6606
MARCO CEBRIAN MARIRRODRIGA <marcocebrian55@gmail.com>
2025-12-28 12:21:01 +0000

    arbol_cebrian

--- src/js/components/Arbol.jsx
+++ src/js/components/Arbol.jsx
@@ -1,11 +1,50 @@
+
+import React, { useState, useEffect } from 'react';
+
 export default function Arbol() {
+const [luzActiva, setactivarLuz] = useState(()=>Array(10).fill(false));
+useEffect(()=>{
+  const interval= setInterval(()=>{
+    const lucesactivadas = Array(luzActiva.length).fill(false);
+    const lucesAleatorias= Math.floor(Math.random()*lucesactivadas.length);
+    lucesactivadas[lucesAleatorias]=true;
+    setactivarLuz(lucesactivadas)
+     
+  },500);
+
+  return()=> clearInterval(interval);
+});
+
   return (
+    
+
+
     <div className="tree">
       {/* Hojas del árbol */}
       <div className="tri layer-1" />
       <div className="tri layer-2" />
       <div className="tri layer-3" />
 
+      <div className="luces">
+        <div className="fila-luces">
+          <div className={`luz roja ${luzActiva[0] ? 'is-on' : ''}`}>●</div>
+        </div>
+        <div className="fila-luces">
+          <div className={`luz amarilla ${luzActiva[1] ?'is-on' : ''}`}>●</div>
+          <div className={`luz verde ${luzActiva[2] ?'is-on' : ''}`}>●</div>
+        </div>
+        <div className="fila-luces">
+          <div className={`luz verde ${luzActiva[3] ?'is-on' : ''}`}>●</div>
+          <div className={`luz amarilla ${luzActiva[4] ?'is-on' : ''}`}>●</div>
+          <div className={`luz roja ${luzActiva[5] ?'is-on' : ''}`}>●</div>
+        </div>
+        <div className="fila-luces">
+          <div className={`luz roja ${luzActiva[6] ?'is-on' : ''}`}>●</div>
+          <div className={`luz verde ${luzActiva[7] ?'is-on' : ''}`}>●</div>
+          <div className={`luz amarilla ${luzActiva[8] ?'is-on' : ''}`}>●</div>
+          <div className={`luz roja ${luzActiva[9] ?'is-on' : ''}`}>●</div>
+        </div>
+      </div>
       {/* Luces del árbol */}
 
       <div className="trunk" />

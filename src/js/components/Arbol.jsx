10341fbcca75fd54b2c92f8095d437d88c31b42e
MARCO CEBRIAN MARIRRODRIGA <marcocebrian55@gmail.com>
2026-01-05 16:19:47 +0000

    segundo guardado

--- src/js/components/Arbol.jsx
+++ src/js/components/Arbol.jsx
@@ -1,22 +1,31 @@
 
 import React, { useState, useEffect } from 'react';
 
-export default function Arbol() {
-const [luzActiva, setactivarLuz] = useState(()=>Array(10).fill(false));
-useEffect(()=>{
-  const interval= setInterval(()=>{
-    const lucesactivadas = Array(luzActiva.length).fill(false);
-    const lucesAleatorias= Math.floor(Math.random()*lucesactivadas.length);
-    lucesactivadas[lucesAleatorias]=true;
-    setactivarLuz(lucesactivadas)
-     
-  },500);
+const Arbol =() => {
+  const [luzActiva, setactivarLuz] = useState(() => Array(10).fill(false));
+  const [funcionando, setFuncionando] = useState(true)
+  useEffect(() => {
+    if (funcionando === true) {
 
-  return()=> clearInterval(interval);
-});
+    const interval = setInterval(() => {
+      const lucesactivadas = Array(luzActiva.length).fill(false);
+      const lucesAleatorias = Math.floor(Math.random() * lucesactivadas.length);
+      lucesactivadas[lucesAleatorias] = true;
+      setactivarLuz(lucesactivadas)
+
+    }, 500);
+
+    return () => clearInterval(interval);
+  }
+  }, [funcionando]);
+
+  const apagarLuces = () => {
+    setactivarLuz(lucesactivadas => lucesactivadas.map(() => false))
+    setFuncionando(false)
+  }
 
   return (
-    
+
 
 
     <div className="tree">
@@ -30,24 +39,34 @@ useEffect(()=>{
           <div className={`luz roja ${luzActiva[0] ? 'is-on' : ''}`}>●</div>
         </div>
         <div className="fila-luces">
-          <div className={`luz amarilla ${luzActiva[1] ?'is-on' : ''}`}>●</div>
-          <div className={`luz verde ${luzActiva[2] ?'is-on' : ''}`}>●</div>
+          <div className={`luz amarilla ${luzActiva[1] ? 'is-on' : ''}`}>●</div>
+          <div className={`luz verde ${luzActiva[2] ? 'is-on' : ''}`}>●</div>
         </div>
         <div className="fila-luces">
-          <div className={`luz verde ${luzActiva[3] ?'is-on' : ''}`}>●</div>
-          <div className={`luz amarilla ${luzActiva[4] ?'is-on' : ''}`}>●</div>
-          <div className={`luz roja ${luzActiva[5] ?'is-on' : ''}`}>●</div>
+          <div className={`luz verde ${luzActiva[3] ? 'is-on' : ''}`}>●</div>
+          <div className={`luz amarilla ${luzActiva[4] ? 'is-on' : ''}`}>●</div>
+          <div className={`luz roja ${luzActiva[5] ? 'is-on' : ''}`}>●</div>
         </div>
         <div className="fila-luces">
-          <div className={`luz roja ${luzActiva[6] ?'is-on' : ''}`}>●</div>
-          <div className={`luz verde ${luzActiva[7] ?'is-on' : ''}`}>●</div>
-          <div className={`luz amarilla ${luzActiva[8] ?'is-on' : ''}`}>●</div>
-          <div className={`luz roja ${luzActiva[9] ?'is-on' : ''}`}>●</div>
+          <div className={`luz roja ${luzActiva[6] ? 'is-on' : ''}`}>●</div>
+          <div className={`luz verde ${luzActiva[7] ? 'is-on' : ''}`}>●</div>
+          <div className={`luz amarilla ${luzActiva[8] ? 'is-on' : ''}`}>●</div>
+          <div className={`luz roja ${luzActiva[9] ? 'is-on' : ''}`}>●</div>
         </div>
       </div>
       {/* Luces del árbol */}
 
       <div className="trunk" />
+
+
+      <button className='Boton' onClick={apagarLuces}>
+        APAGAR
+      </button>
+      <button className='Boton' onClick={() => setFuncionando(true)}>
+        ENCENDER
+      </button>
     </div>
+
   );
 }
+export default Arbol;
